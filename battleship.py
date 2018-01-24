92b19c6bbb3b17ba466978b33d99cd5593235518
Christopher Sardegna <github@reagentx.net>
2018-01-23 23:39:14 -0800

    Fix some broken code

--- battleship.py
+++ battleship.py
@@ -1,8 +1,9 @@
 from random import randint
 
 board = []
-for x in range(5):
-    board.append(["O"] * 5)
+
+for x in range(6):
+    board.append(["O"] * 4)
 
 
 def print_board(board):
@@ -10,11 +11,6 @@ def print_board(board):
         print " ".join(row)
 
 
-def d():
-    pass
-
-
-print "Let's play Battleship!"
 print_board(board)
 
 
@@ -28,23 +24,35 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+# To cheat, uncomment these:
+# print ship_row
+# print ship_col
 
-for misses in range(4):
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+
+# Everything from here on should go in your for loop!
+def check_status(guess_row, guess_col):
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
-        board[guess_row][guess_col] = "#"
-    misses = 3
+        board[guess_row][guess_col] = "@"
+        print_board(board)
+    elif guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+    elif(board[guess_row][guess_col] == "X"):
+        print "You guessed that one already."
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
-        elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
-        else:
-            print "You missed my battleship!"
-            board[guess_row][guess_col] = "X"
-    print "Turn ", misses + 1
-    print_board(board)
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+        print_board(board)
+
+
+misses = 0
+while misses < 4:
+    misses += 1
+    print "Move: %s" % misses
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Col: "))
+    check_status(guess_row, guess_col)
+    if guess_row == ship_row and guess_col == ship_col:
+        break
+
+print "Game Over"
